01c4437363003c0d879357c822f8e8e04f0e749e
sakshee <sakshee@gmail.com>
2026-06-25 21:05:20 +0530

    fix whitenoise static

--- crop_recognition_system/settings.py
+++ crop_recognition_system/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #Default primary key field type
 #https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
